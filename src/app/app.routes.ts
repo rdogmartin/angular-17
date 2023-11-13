bc376743c5e355085337bf3d994a7ecf3bd47f71
Roger Martin <rdogmartin@gmail.com>
2023-11-13 16:29:03 -0700

    Add ShoppingList component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './pages/shopping-list/shopping-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    title: 'Home page',
+  },
+  {
+    path: 'shopping-list',
+    component: ShoppingListComponent,
+    title: 'Shopping List',
+  },
+];
