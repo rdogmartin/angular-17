803f17a84b2eb200913d6e631e924080073bc38f
Roger Martin <rdogmartin@gmail.com>
2023-11-15 09:49:15 -0700

    Add Angular Material

--- src/app/pages/shopping-list/shopping-list.component.ts
+++ src/app/pages/shopping-list/shopping-list.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-shopping-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCheckboxModule],
   templateUrl: './shopping-list.component.html',
-  styleUrl: './shopping-list.component.scss'
+  styleUrl: './shopping-list.component.scss',
 })
-export class ShoppingListComponent {
-
-}
+export class ShoppingListComponent {}
